926c2aafd689b73f19c7215a9dc80ea7f6307802
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-12-26 17:09:13 +0700

    update js/script.js

--- js/script.js
+++ js/script.js
@@ -3,28 +3,27 @@ const productsData = [
     "id": 1,
     "category": "coal",
     "title": "Coal",
-    "preview": "Coal is a combustible black or brownish-black sedimentary rock"
+    "preview": "Coal is a combustible black or brownish-black sedimentary rock",
     "description": "Coal is a combustible black or brownish-black sedimentary rock, formed as rock strata called coal seams. Coal is mostly carbon with variable amounts of other elements, chiefly hydrogen, sulfur, oxygen, and nitrogen.",
     "imageUrl": "https://images.unsplash.com/photo-1523848309072-c199db53f137?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     "id": 2,
     "category": "kopi",
-    "title": "Coffe",
-    "description": "A coffee bean is a fruit from the Coffea plant and the source for coffee. It is the pip inside the red or purple fruit. This fruit is often referred to as a coffee cherry, and like the cherry, it is a stone fruit.",
+    "title": "Coffee",
+    "preview": "A coffee bean is a fruit from the Coffea plant and the source for coffee. It is the pip inside the red or purple fruit. This fruit is often referred to as a coffee cherry, and like the cherry, it is a stone fruit.",
+    "description": "A coffee bean is a seed of the Coffea plant and the source for coffee. It is the pit inside the red or purple fruit. This fruit is often called a cherry, and like the cherry, it is a stone fruit.",
     "imageUrl": "https://images.unsplash.com/photo-1702430179297-4d9825e4b827?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     "id": 3,
-    "category": "Briquette",
+    "category": "briquette",
     "title": "Briquette",
+    "preview": "Briquette is a compressed block of coal dust or other combustible biomass material used for fuel and kindling to start a fire.",
     "description": "Briquette is a compressed block of coal dust or other combustible biomass material used for fuel and kindling to start a fire.",
     "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Charcoal2.jpg/800px-Charcoal2.jpg"
   }
-]
-
-
-
+];
 
 function searchProducts() {
     const input = document.getElementById("searchInput").value.toLowerCase();
@@ -39,7 +38,6 @@ function searchProducts() {
         }
     });
 }
-
 
 // Fungsi untuk menghasilkan kartu produk
 function generateProductCards(products) {
@@ -65,23 +63,16 @@ function generateProductCards(products) {
     });
 }
 
-
 function showProductDetails(productId) {
-  fetch("/js/data.js")
-    .then(response => response.json())
-    .then(productsData => {
-      const selectedProduct = productsData.find((product) => product.id === productId);
+  const selectedProduct = productsData.find((product) => product.id === productId);
 
-      const modalBody = document.getElementById("productModalBody");
-      modalBody.innerHTML = `
-        <img src="${selectedProduct.imageUrl}" class="card-img-top mb-3" alt="${selectedProduct.title}">
-        <h5 class="modal-title">${selectedProduct.title}</h5>
-        <p>${selectedProduct.description}</p>
-      `;
+  const modalBody = document.getElementById("productModalBody");
+  modalBody.innerHTML = `
+    <img src="${selectedProduct.imageUrl}" class="card-img-top mb-3" alt="${selectedProduct.title}">
+    <h5 class="modal-title">${selectedProduct.title}</h5>
+    <p>${selectedProduct.description}</p>
+  `;
 
-      const productModal = new bootstrap.Modal(document.getElementById('productModal'));
-      productModal.show();
-    });
+  const productModal = new bootstrap.Modal(document.getElementById('productModal'));
+  productModal.show();
 }
-
-
